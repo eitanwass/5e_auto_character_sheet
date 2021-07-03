bb501fe27ba18e33080837753631f43d866573fd
eitanwass <eitanwass@gmail.com>
2021-07-03 11:57:52 +0300

    Added editting abilities values

--- src/components/abilityComponents/AbilitiesPanel.tsx
+++ src/components/abilityComponents/AbilitiesPanel.tsx
@@ -1,11 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import _ from "lodash";
-import { Card, Grid, makeStyles } from "@material-ui/core";
+
+import { Card, Grid, IconButton, makeStyles } from "@material-ui/core";
+import { Edit } from "@material-ui/icons";
 
 import AbilityScore from "./AbilityScore";
 import AbilityChecks from "./AbilityChecks";
 import { abilitiesProperties } from "../../consts/abilitiesConsts";
 import { CharacterContext } from "../../contexts/CharacterContext";
+
+import "./AbilitiesPanel.sass";
 
 
 const useStyles = makeStyles({
@@ -26,22 +30,29 @@ const useStyles = makeStyles({
 const AbilitiesPanel = (): JSX.Element => {
 	const classes = useStyles();
 	const { abilityScores } = useContext(CharacterContext);
+	const [isEditable, setEditable] = useState<boolean>(false);
+	const toggleEditable = () => setEditable(!isEditable);
 
 	return (
-		<Card className={classes.card}>
-			{
-				_.map(abilitiesProperties, ({ name, checks }) =>
-					<Grid container xs={12} spacing={2}>
-						<Grid item xs={5}>
-							<AbilityScore label={name} value={abilityScores.getter[name]}/>
+		<>
+			<IconButton className={"toggle-edit-button"} onClick={toggleEditable}>
+				<Edit color={isEditable ? "action" : "primary"} fontSize={"small"}/>
+			</IconButton>
+			<Card className={classes.card}>
+				{
+					_.map(abilitiesProperties, ({ name, checks }) =>
+						<Grid container xs={12} spacing={2}>
+							<Grid item xs={5}>
+								<AbilityScore label={name} value={abilityScores.getter[name]} isEditable={isEditable} />
+							</Grid>
+							<Grid item xs={7} className={classes.abilityChecks}>
+								<AbilityChecks abilityName={name} checks={checks}/>
+							</Grid>
 						</Grid>
-						<Grid item xs={7} className={classes.abilityChecks}>
-							<AbilityChecks abilityName={name} checks={checks}/>
-						</Grid>
-					</Grid>
-				)
-			}
-		</Card>
+					)
+				}
+			</Card>
+		</>
 	);
 };
 
--- src/components/abilityComponents/AbilityScore.tsx
+++ src/components/abilityComponents/AbilityScore.tsx
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { MouseEventHandler, useContext } from "react";
 import _ from "lodash";
 
-import { valueToModifier } from "../../utils";
-
 import "./AbilityScore.css";
+import { IconButton, SvgIconTypeMap } from "@material-ui/core";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+
+import { valueToModifier } from "../../utils";
+import { CharacterContext } from "../../contexts/CharacterContext";
 
 
 interface AbilityScoreType {
 	label: string;
 	value: number;
+	isEditable: boolean;
 }
 
-const AbilityScore = ({ label, value }: AbilityScoreType): JSX.Element => {
+const ModifyButton = ({
+	action,
+	Icon
+}: { action: MouseEventHandler<HTMLButtonElement>, Icon: OverridableComponent<SvgIconTypeMap> }): JSX.Element =>
+	<IconButton style={{ padding: "0 0.1rem" }} onClick={action}><Icon color={"primary"}/></IconButton>;
+
+
+const AbilityScore = ({ label, value, isEditable }: AbilityScoreType): JSX.Element => {
+	const { abilityScores } = useContext(CharacterContext);
 	const abilityScoreModifier = valueToModifier(value);
 	const compiledLabel = _.toUpper(label.substring(0, 3));
+
+	const modifyAction = (amount: number): void =>
+		abilityScores.setter({ ...abilityScores.getter, [label]: abilityScores.getter[label] + amount });
 
 	return (
 		<div className={"ability-scores"}>
@@ -21,7 +37,11 @@ const AbilityScore = ({ label, value }: AbilityScoreType): JSX.Element => {
 				<span className={"ability-name"}>{compiledLabel}</span>
 				<span className={"ability-value"}>{value}</span>
 			</div>
-			<div className={"ability-modifier"}>{`${0 <= abilityScoreModifier ? "+" : ""}${abilityScoreModifier}`}</div>
+			<div className={"ability-modifier"}>
+				{isEditable && <ModifyButton action={() => modifyAction(1)} Icon={KeyboardArrowUp}/>}
+				{`${0 <= abilityScoreModifier ? "+" : ""}${abilityScoreModifier}`}
+				{isEditable && <ModifyButton action={() => modifyAction(-1)} Icon={KeyboardArrowDown}/>}
+			</div>
 		</div>
 	);
 };
