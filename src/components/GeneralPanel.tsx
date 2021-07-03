c1468839e878fd7eca4555b34c99e2ec91fe3f5c
eitanwass <eitanwass@gmail.com>
2021-07-03 14:26:25 +0300

    Use new tablist, and added tab body

--- src/components/GeneralPanel.tsx
+++ src/components/GeneralPanel.tsx
@@ -1,23 +1,41 @@
 import React, { useState } from "react";
-import { Tab, Tabs } from "@material-ui/core";
+import { Card, Tab, Tabs } from "@material-ui/core";
+import { TabContext, TabList, TabPanel } from "@material-ui/lab";
 
 
 const GeneralPanel = (): JSX.Element => {
-	const [currentTab, setCurrentTab] = useState<number>(0);
+	const [currentTab, setCurrentTab] = useState<string>("0");
 
 	return (
-		<Tabs
-			value={currentTab}
-			onChange={(event: React.ChangeEvent<Record<string, unknown>>, newValue: number) => setCurrentTab(newValue)}
-			variant={"scrollable"}
-			scrollButtons={"auto"}
-			centered>
-			<Tab label={"Character"}/>
-			<Tab label={"Stats"}/>
-			<Tab label={"Features"}/>
-			<Tab label={"Equipment"}/>
-			<Tab label={"Spells"}/>
-		</Tabs>
+		<TabContext value={currentTab}>
+			<Card>
+				<TabList
+					onChange={(event: React.ChangeEvent<Record<string, unknown>>, newTab: string) => setCurrentTab(newTab)}
+					variant={"scrollable"}
+					scrollButtons={"auto"}>
+					<Tab label={"Character"} value={"0"} />
+					<Tab label={"Stats"} value={"1"} />
+					<Tab label={"Features"} value={"2"} />
+					<Tab label={"Equipment"} value={"3"} />
+					<Tab label={"Spells"} value={"4"} />
+				</TabList>
+			</Card>
+			<TabPanel value={"0"}>
+				Tab 0
+			</TabPanel>
+			<TabPanel value={"1"}>
+				Tab 1
+			</TabPanel>
+			<TabPanel value={"2"}>
+				Tab 2
+			</TabPanel>
+			<TabPanel value={"3"}>
+				Tab 3
+			</TabPanel>
+			<TabPanel value={"4"}>
+				Tab 4
+			</TabPanel>
+		</TabContext>
 	);
 };
 
